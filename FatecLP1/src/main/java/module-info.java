460b6375a63cc75b521c957fd3ba02a5f024cacd
lucamusilva2014 <lucamusilva2014@gmail.com>
2025-05-20 22:27:03 -0300

    Delete FatecLP1 directory

--- FatecLP1/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens org.example to javafx.fxml;
-    exports org.example;
-    exports org.example.Classe;
-    opens org.example.Classe to javafx.fxml;
-}
